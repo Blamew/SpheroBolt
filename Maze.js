9d63a60f43b4615a6e092784574eebcc1d86df9b
Bryson <bryson.lamew00@gmail.com>
2024-01-24 08:27:39 -0500

    Blue LED

--- Maze.js
+++ Maze.js
@@ -8,9 +8,8 @@
 async function startProgram(){
 await speak("Start", true);	
 await scrollMatrixText('Start', { r: 66, g: 66, b: 66 }, 75, true)    
-
-
-	
-	
-	
+//LED Blue + Movement
+await roll(360, 75, 2)
+await scrollMatrixText('BLUE LED', { r: 0, g: 0, b: 255 }, 75, true) ; await setFrontLed({ r: 0, g: 0, b: 255 }) ; await setBackLed({ r: 0, g: 0, b: 255 }) 
+await setMainLed({ r: 0, g: 0, b: 255 })
 }
